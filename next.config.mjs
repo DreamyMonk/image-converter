f8c08b2ce7526fe402be8fe1a039d753dec8b4f1
Saptrishi Das Biswas <138619155+DreamyMonk@users.noreply.github.com>
2025-03-28 19:52:09 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-devIndicators: false
+const nextConfig = {
+  // Move the option inside the object
+  devIndicators: {
+    buildActivity: false // This specifically hides the building indicator
+    // autoPrerender: false, // Hides the prerender indicator (optional)
+  }
+  // You can add other Next.js config options here if needed
+};
+
 export default nextConfig;
